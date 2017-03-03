73562825187d0300aaf25a454de33cc1e9e87bf3
Heloá Meneses <heloa.meneses@gmail.com>
2017-03-03 17:43:42 -0300

    Refactoring and checking for main function

--- converter.py
+++ converter.py
@@ -4,68 +4,56 @@ import sys
 
 """
 $ python2.7 converter.py <file_with_permissions>
-
 """
 
+def convert_rules(old_object):
+    def decode_rule(raw_rule):
+        res = {}
+        for rule_index in xrange(0, len(raw_rule), 3 ):
+            operator = raw_rule[rule_index+1]
+            if operator == 'eq':
+                new_rule = {raw_rule[rule_index+0]: raw_rule[rule_index+2]}
+                res.update(new_rule)
+            elif operator == 'in':
+                new_rule = {raw_rule[rule_index+0]: {"$in": raw_rule[rule_index+2]}}
+                res.update(new_rule)
+            elif operator == 'has':
+                new_rule = {raw_rule[rule_index+0]: {"$has": raw_rule[rule_index+2]}}
+                res.update(new_rule)
+            else:
+                raise RuntimeError("ERROR: Unrecognized operator:" + operator)
+        return res 
 
-def decode(raw_rule):
     res = {}
-    while len(raw_rule) > 0:
-        old_rule = raw_rule[:3]
-        raw_rule = raw_rule[3:]
-        operator = old_rule[1]
-
-        if operator == 'eq':
-            new_rule = {old_rule[0]: old_rule[2]}
-            res.update(new_rule)
-        elif operator == 'in':
-            new_rule = {old_rule[0]: {"$in": old_rule[2]}}
-            res.update(new_rule)
-        else:
-            print "ERROR: Unrecognized operator:", old_rule[1]
-
-    return res
-
-
-def convert(old_object):
-    res = {}
-
     for obj_key, obj_val in old_object.iteritems():
 
         if obj_val and len(obj_val) > 1:
             or_obj = []
-
             for obj_rule in obj_val:
-                rule = decode(obj_rule)
+                rule = decode_rule(obj_rule)
                 or_obj.append(rule)
             res[obj_key] = {"$or": or_obj}
-
         elif obj_val and len(obj_val) == 1:
             single_rule = obj_val[0]
-            rule = decode(single_rule)
+            rule = decode_rule(single_rule)
             res[obj_key] = rule
         else:
             res[obj_key] = {}
 
     return res
 
+if __name__ == '__main__':
+    def safe_repr(object, context, maxlevels, level):
+        typ = pprint._type(object)
+        if typ is unicode:
+            object = str(object)
+        return pprint._safe_repr(object, context, maxlevels, level)
 
-def safe_repr(object, context, maxlevels, level):
-    typ = pprint._type(object)
-    if typ is unicode:
-        object = str(object)
-    return pprint._safe_repr(object, context, maxlevels, level)
-
-printer = pprint.PrettyPrinter()
-printer.format = safe_repr
+    printer = pprint.PrettyPrinter()
+    printer.format = safe_repr
 
 
-def read(permission_list):
-    with open(permission_list) as f:
+    with open(sys.argv[1]) as f:
         for line in f:
             json_obj = json.loads(line)
-            printer.pprint(convert(json_obj))
-    return "Finished"
-
-
-read(sys.argv[1])
+            printer.pprint(convert_rules(json_obj))
