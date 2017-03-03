0819518fecff19b79bb414761462960b9e5a8f43
Heloá Meneses <heloa.meneses@gmail.com>
2017-03-03 13:19:13 -0300

    Working with equal operator

--- converter.py
+++ converter.py
@@ -1,18 +1,30 @@
 import pprint
 
-def convert_fn(old_object):
+def decode(raw_rule):
+  res = {}
+  while len(raw_rule) > 0:
+    old_rule = raw_rule[:3]
+    raw_rule = raw_rule[3:]
+
+    if old_rule[1] == 'eq':
+      new_rule = {old_rule[0]: old_rule[2]}
+      res.update(new_rule)
+    else:
+      print "ERROR: Unrecognized operator:",  old_rule[1]
+
+  return res
+
+def convert(old_object):
   res = {}
 
   for obj_key, obj_val in old_object.iteritems():
-    print obj_key, obj_val
     
     if obj_val and len(obj_val) > 1:
       or_obj = []
-      for obj_rule in obj_val:
-        new_rule = {}
-        new_rule[obj_key] = {}
 
-        or_obj.append(new_rule)
+      for obj_rule in obj_val:
+        rule = decode(obj_rule)
+        or_obj.append(rule)
       res[obj_key] = { '$or': or_obj }
     else:
       res[obj_key] = {}
@@ -47,4 +59,5 @@ val = {
   "U40": [], 
   "8": []
 }
-pprint.pprint (convert_fn(val))
+
+pprint.pprint(convert(val))
